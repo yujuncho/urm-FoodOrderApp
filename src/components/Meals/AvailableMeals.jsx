da1dd9a4b66cb10467b6d23b1bfa482d53cdd067
Yujun <yujuncho7@gmail.com>
2021-07-13 13:46:24 -0700

    Set up meals data to be fetched from a backend and added a basic loading and error state

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,37 +1,60 @@
+import { useState, useEffect } from "react";
+
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
 
 import styles from "./AvailableMeals.module.css";
 
-const MOCK_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99
-  }
-];
+const API_URL = "";
 
 function AvailableMeals() {
-  let mealItemList = MOCK_MEALS.map(meal => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const getMeals = async () => {
+      const response = await fetch(API_URL);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const responseData = await response.json();
+
+      let loadedMeals = [];
+
+      for (let key in responseData) {
+        loadedMeals.push({ id: key, ...responseData[key] });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    getMeals().catch(error => {
+      setHttpError(error.message);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  let mealItemList = meals.map(meal => {
     return (
       <MealItem
         id={meal.id}
